90c6a7d8c00436fbecc2950ad8d6a70badaffe41
Marianne Linardos <mlinardos.edu@gmail.com>
2019-07-31 10:08:11 -0400

    Add project files.

---
+++ Interfacev2Proj/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Interfacev2Proj {
+    class Program {
+        static void Main(string[] args) {
+           
+
+
+
+        }
+    }
+}
---
+++ Interfacev2Proj/Service.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Interfacev2Proj {
+    class Service {
+
+
+    }
+}
